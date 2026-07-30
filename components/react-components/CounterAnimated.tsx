2a608b9b265b8688a1292506d7f76c5d945e5b90
Max <zmax1301@gmail.com>
2026-07-30 21:47:57 +0300

    dollar_fix

--- components/react-components/CounterAnimated.tsx
+++ components/react-components/CounterAnimated.tsx
@@ -5,7 +5,7 @@ import CountUp from "react-countup";
 export default function CounterAnimated({ amount }: { amount: number }) {
     return (
         <CountUp
-            prefix="₴ "
+            prefix="$ "
             decimal=","
             decimals={2}
             duration={1.5}
